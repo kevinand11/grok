8e327f2b0094e6e5f045af0c3f62f1a8276583a2
kevinand11 <kevinfizu@gmail.com>
2023-11-30 15:19:30 +0100

    gh-pages

---
+++ _nuxt/app.config.32a9b839.js
@@ -0,0 +1,1 @@
+import{i as u,B as s}from"./entry.6111a3c0.js";function t(e,n){return u()._useHead(e,n)}const i={};s(i);export{t as u};
--- src/application/constants/index.ts
+++
@@ -1,3 +0,0 @@
-export const TwitterLink = 'https://twitter.com/KORGErc20'
-export const TelegramLink = 'https://t.me/KORG_Portal'
-export const ContractAddress = '(Contract Address - 0xdC514980786145f63d7de5631FfB30264238f300)'
